bc0ee3d03eee73555889af9765190fe08a69e91c
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-08-09 12:21:48 +0200

    cambiado

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php   
-       
+    //Crea un archivo de texto para guardar los datos que envía el ESP8266
+    if (!file_exists("miTemp&Hum.txt")){
+        // si no existe el archivo, lo creamos
+        file_put_contents("miTemp&Hum.txt", "0.0\r\n0.0");
+    }
+    
     // Si se recibe Datos con el Método GET, los procesamos
-    if (isset($_GET['contador_esp'])){
-        $var1 = $_GET['contador_esp'];
-        echo $var1;
+    if (isset($_GET['Temp']) || isset($_GET['Hum'])){
+        $var3 = $_GET['Temp'];
+        $var4 = $_GET['Hum'];
+        $fileContent = $var3 . "\r\n" . $var4;
+        $fileSave = file_put_contents("miTemp&Hum.txt", $fileContent);
     }
    
-   
+    // Leemos los datos del archivo para guardarlos en variables
+    $fileStr = file_get_contents("miTemp&Hum.txt");
+    $pos1 = strpos($fileStr, "\r\n");
+    $var1 = substr($fileStr, 0, $pos1);
+    $var2 = substr($fileStr, $pos1 + 1); // de la pos1 +1 hasta el final
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +25,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="3">
-    <title>Luz</title>
+    --<meta http-equiv="refresh" content="3">
+    <title>Control de nivel de agua</title>
 
 </head>
     <style>
@@ -27,11 +38,12 @@
     </style>
 <body>
     <header>
-        <h1>Servidor de datos para ver luz</h1>
+        <h1>BIENVENIDOS AL SERVIDOR REMOTO</h1>
     </header>
     
     <section>
         <h3>Nivel: <?php echo $var1; ?></h3>
+        <h3>Nivel: <?php echo $var2; ?></h3>
     </section>
 </body>
 </html>
